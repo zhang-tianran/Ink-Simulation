d1407e919d4a59b39c7c317aeee9573fc540e36b
gizmo1479 <gizmo1479@system76-pc.localdomain>
2023-04-13 16:53:27 -0400

    basic main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
+#include <iostream>
+#include <QImage>
+
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addPositionalArgument("scene", "Scene file to be rendered");
+    parser.addPositionalArgument("output", "Image file to write the rendered image to");
+
+    parser.process(a);
+
+    // const QStringList args = parser.positionalArguments();
+    // if(args.size() != 2) {
+    //     std::cerr << "Error: Wrong number of arguments" << std::endl;
+    //     a.exit(1);
+    //     return 1;
+    // }
+    // QString scenefile = args[0];
+    // QString output = args[1];
+
+    // QImage image(IMAGE_WIDTH, IMAGE_HEIGHT, QImage::Format_RGB32);
+
+
+    // QRgb *data = reinterpret_cast<QRgb *>(image.bits());
+
+    // tracer.traceScene(data, *scene);
+    // delete scene;
+
+    // bool success = image.save(output);
+    // if(!success) {
+    //     success = image.save(output, "PNG");
+    // }
+    // if(success) {
+    //     std::cout << "Wrote rendered image to " << output.toStdString() << std::endl;
+    // } else {
+    //     std::cerr << "Error: failed to write image to " << output.toStdString() << std::endl;
+    // }
+    a.exit();
+}
